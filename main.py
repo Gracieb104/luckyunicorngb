4b345fb3885dbeae87ae10a55d3b02b5868cab62
Gracieb104 <107890786+Gracieb104@users.noreply.github.com>
2022-06-21 09:25:25 +1200

    skelton of the base code

---
+++ main.py
@@ -0,0 +1,18 @@
+
+base compontes for the game lucky unicorn
+
+
+    authour - gracie bartosh
+cc ws 2022
+
+
+
+#important libarines below the line ***************
+
+
+#all funcations below the line ************8
+
+
+
+#main  route below this line **************
+print("hello")
